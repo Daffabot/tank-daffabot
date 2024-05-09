70c9239e578c5625ca354f0267769848471d1b66
Daffabot <daffabot@programmer.net>
2024-05-09 11:08:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -995,12 +995,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
         ///^^^^^^^///////////^^^^/////////
 
+        let infoButton = document.getElementById('info');
+        let startBtn = document.getElementById('start');
+        let btnLeft = document.getElementById('btn-left');
+        let btnRight = document.getElementById('btn-right');
+        infoButton.addEventListener('click', inf);
+        startBtn.addEventListener('click', startGame);
+        btnLeft.addEventListener('click', () => changeLvl(0));
+        btnRight.addEventListener('click', () => changeLvl(1));
+
         function loader() {
             let maindiv = document.getElementById("main");
             if (lvl < 0 || lvl > 9) {
                 lvl = 0;
             }
-            maindiv.innerHTML = '<button id="start" onclick="startGame()">Mulai</button><button id="info" onclick="inf()">Info</button><br/><span class="dif">Kesulitan:</span><br/><button id="btn-left" onclick="changeLvl(0)">&lt</button><span class="dif" >' + dif[lvl] + '  </span><button id="btn-right" onclick="changeLvl(1)" >&gt</button>';
+            maindiv.innerHTML = '<button id="start">Mulai</button><button id="info">Info</button><br/><span class="dif">Kesulitan:</span><br/><button id="btn-left">&lt</button><span class="dif" >' + dif[lvl] + '  </span><button id="btn-right">&gt</button>';
             lastTime = Date.now();
         };
         //////////////////////////////////
